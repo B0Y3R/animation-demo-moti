2220b2f1606983dbbb533f84578b7786ff1294ce
James Boyer <jamesboyer92@gmail.com>
2022-07-17 19:47:30 -0400

    working on home cards

--- src/Navigation/RootNavigation.js
+++ src/Navigation/RootNavigation.js
@@ -4,7 +4,10 @@ import { AntDesign } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import {View, Text, TouchableOpacity } from 'react-native'
+import { useMemo } from 'react';
+import { Easing } from 'react-native-reanimated'
 
+import { MotiPressable } from 'moti/interactions'
 
 import RippleEffect from '../components/RippleEffect';
 import BreathingLoader from '../components/BreathingLoader';
@@ -31,23 +34,46 @@ const pageData = [
     },
 ]
 
+const BREATHING_LOADER_FROM = { scale: 1, borderWidth: 5, opacity: 0.9 };
+const BREATHING_LOADER_ANIMATE = {  scale: 1.2, borderWidth: 8, opacity: 1 }
+const BREATHING_LOADER_TRANSITION = (duration) => ({
+    type: 'timing',
+    duration: duration,
+    easing: Easing.out(Easing.ease),
+    loop: true
+});
+
 function Home({navigation}) {
     return (
-        <View style={{ flex: 1, display: 'flex', flexDirection: 'column', width: '100%', height: '100%', backgroundColor: '#003366'}}>
+        <View style={{ flex: 1, display: 'flex', flexDirection: 'column', width: '100%', height: '100%',         backgroundColor: '#003366',}}>
             <View style={{ padding: 20, display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
                 {
-                    pageData.map(page => (
-                        <TouchableOpacity
+                    pageData.map((page, index) => (
+                        <MotiPressable
+                            key={index}
+                            animate={{ rotateY: "-10deg" }}
+                            from={{ rotateY: "5deg" }}
+                            transition={{
+                                type: 'timing',
+                                duration: 2000,
+                                easing: Easing.out(Easing.ease),
+                                loop: true
+                            }}
                             style={{
                                 height: 100,
                                 width: 100,
                                 margin: 8,
                                 backgroundColor: 'rgba(255, 255, 255, 0.1)',
                                 borderRadius: 20,
+                                borderColor: 'transparent',
                                 display: 'flex',
                                 flexDirection: 'column',
                                 justifyContent: 'center',
-                                alignItems: 'center'
+                                alignItems: 'center',
+                                shadowColor: 'black',
+                                shadowOffset: { width: 0, height: 0 },
+                                shadowOpacity: 1,
+                                shadowRadius: 5,
                             }}
                             onPress={() => navigation.navigate(page.route)}
                         >
@@ -62,7 +88,7 @@ function Home({navigation}) {
                             >
                                 {page.title}
                             </Text>
-                        </TouchableOpacity>
+                        </MotiPressable>
                     ))
                 }
             </View>
